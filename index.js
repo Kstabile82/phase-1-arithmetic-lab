a260c87e41ad16391ce0a7501c6357fc2b4f7b4d
KarinaStabile <karina.stabile@gmail.com>
2021-08-09 23:18:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31; 
+let num2 = 2; 
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 200) + 1; 
+let num3 = 24; 
+let num4 = 5; 
+let mod = num3 % num4;  
+let max = Math.max(1, 3, 20, 17, 14); 
 
-// Write your code here
